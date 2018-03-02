988e228efc22265dad800c6b24fc4351f76be0b1
becknian <32223253+becknian@users.noreply.github.com>
2018-03-02 12:30:15 -0500

    Add files via upload

---
+++ week4/crawler.py
@@ -0,0 +1,17 @@
+import scrapy
+
+class Crawler(scrapy.Spider):
+	name='spider-repositories'
+	def start_requests(self):
+		url = 'https://github.com/shiyanlou?page={}&tab=repositories'
+		urls = (url.format(i) for i in range(1, 4))
+
+		for url in urls:
+			yield scrapy.Request(url=url, callback=self.parse)
+
+	def parse(self, response):
+		for repository in response.css('li.col-12'):
+			yield {
+				'name':repository.css('a::text').re_first('[^\w]*(.*)[^\w]*'),
+				'update_time':repository.css('relative-time::attr(datetime)').extract_first()
+			}
