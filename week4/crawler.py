cf4cade1a4edccae9130fff67fe07ddf22f14654
becknian <32223253+becknian@users.noreply.github.com>
2018-03-03 03:57:58 +0000

    Update crawler.py

--- week4/crawler.py
+++ week4/crawler.py
@@ -3,8 +3,8 @@ import scrapy
 class Crawler(scrapy.Spider):
 	name='spider-repositories'
 	def start_requests(self):
-		url = 'https://github.com/shiyanlou?page={}&tab=repositories'
-		urls = (url.format(i) for i in range(1, 5))
+		git_url = 'https://github.com/shiyanlou?page={}&tab=repositories'
+		urls = (git_url.format(i) for i in range(1, 5))
 
 		for url in urls:
 			yield scrapy.Request(url=url, callback=self.parse)
@@ -14,4 +14,4 @@ class Crawler(scrapy.Spider):
 			yield {
 				'name':repository.css('a::text').re_first('[^\w]*(.*)[^\w]*'),
 				'update_time':repository.css('relative-time::attr(datetime)').extract_first()
-			}
+}
